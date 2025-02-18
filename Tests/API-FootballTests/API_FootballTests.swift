2410e561592a24da40fc2d0d95d3f4cc6e43e240
shawon1fb <shawon0187@gmail.com>
2025-02-18 20:44:33 +0600

    Initial Commit

---
+++ Tests/API-FootballTests/API_FootballTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import API_Football
+
+final class API_FootballTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
